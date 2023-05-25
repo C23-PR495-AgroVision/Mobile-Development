7f51e23591d6fc347be6d4bf9ddf34fde9b38802
fariabyd <118955022+fariabyd@users.noreply.github.com>
2023-05-25 13:12:54 +0700

    up splashscreen

--- app/src/main/java/com/fari/agrovision/MainActivity.kt
+++ app/src/main/java/com/fari/agrovision/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.fari.agrovision
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.fari.agrovision.databinding.ActivityMainBinding
+import com.fari.agrovision.ui.home.HomeFragment
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+
+        Timer().schedule(timerTask {
+            val moveToHomeFragment = Intent(this@MainActivity, HomeFragment::class.java)
+            startActivity(moveToHomeFragment)
+            finish()
+        }, timerSplashScreen)
+    }
+
+    companion object {
+        const val timerSplashScreen = 3000L
     }
 }
