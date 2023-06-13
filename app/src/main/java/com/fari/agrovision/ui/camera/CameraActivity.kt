7158eded26b12fa9e3e8b556007aeb969cfeedf4
fariabyd <118955022+fariabyd@users.noreply.github.com>
2023-06-14 04:19:44 +0700

    up camera

--- app/src/main/java/com/fari/agrovision/ui/camera/CameraActivity.kt
+++ app/src/main/java/com/fari/agrovision/ui/camera/CameraActivity.kt
@@ -1,12 +1,194 @@
 package com.fari.agrovision.ui.camera
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import com.fari.agrovision.R
+import com.fari.agrovision.data.remote.utils.createFile
+import com.fari.agrovision.data.remote.utils.uriToFile
+import com.fari.agrovision.databinding.ActivityCameraBinding
 
 class CameraActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCameraBinding
+
+    private var isDetection = true
+    private var isDiseaseRipe = true
+    private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+    private var imageCapture: ImageCapture? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_camera)
+        binding = ActivityCameraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getCondition()
+
+        setupAction()
+    }
+
+    private fun getCondition() {
+        isDetection = intent.getBooleanExtra(IS_DETECTION, true)
+        isDiseaseRipe = intent.getBooleanExtra(IS_DISEASE_RIPE, true)
+    }
+
+    private fun setupAction() {
+        binding.apply {
+            btnCamera.setOnClickListener { takePhoto() }
+            btnSwitchCamera.setOnClickListener { startCamera() }
+            btnGallery.setOnClickListener { startGallery() }
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        startCamera()
+        hideSystemUI()
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+        val photoFile = createFile(application)
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Toast.makeText(
+                        this@CameraActivity,
+                        "Gagal mengambil gambar",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val intent = Intent(this@CameraActivity, ImagePreviewActivity::class.java)
+                    intent.putExtra(ImagePreviewActivity.IMAGE_PATH, photoFile.absolutePath)
+                    intent.putExtra(ImagePreviewActivity.IS_DETECTION, isDetection)
+                    intent.putExtra(ImagePreviewActivity.IS_DISEASE_RIPE, isDiseaseRipe)
+                    intent.putExtra(
+                        "isBackCamera",
+                        cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA
+                    )
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        )
+    }
+
+    private val launcherIntentGallery =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val selectedImg = result.data?.data as Uri
+                selectedImg.let { uri ->
+                    val imageFile = uriToFile(uri, this@CameraActivity)
+                    val intent = Intent(this@CameraActivity, ImagePreviewActivity::class.java)
+                    intent.putExtra(ImagePreviewActivity.IMAGE_PATH, imageFile.absolutePath)
+                    intent.putExtra(ImagePreviewActivity.IS_DETECTION, isDetection)
+                    intent.putExtra(ImagePreviewActivity.IS_DISEASE_RIPE, isDiseaseRipe)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+            }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            try {
+                cameraProvider.unbindAll()
+                val camera = cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview,
+                    imageCapture
+                )
+
+                val cameraInfo = camera.cameraInfo
+                val hasFlashUnit = cameraInfo.hasFlashUnit()
+
+                if (hasFlashUnit) {
+                    val cameraControl = camera.cameraControl
+
+                    cameraControl.enableTorch(false)
+
+                    binding.btnFlash.setOnClickListener {
+                        val newFlashMode = camera.cameraInfo.torchState.value
+
+                        if (newFlashMode == TorchState.OFF) {
+                            cameraControl.enableTorch(true)
+                            binding.btnFlash.setImageResource(R.drawable.ic_flash_on)
+                        } else {
+                            cameraControl.enableTorch(false)
+                            binding.btnFlash.setImageResource(R.drawable.ic_flash_off)
+                        }
+                    }
+                } else {
+                    Toast.makeText(
+                        this@CameraActivity,
+                        "Perangkat tidak memiliki unit flash",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            } catch (exc: Exception) {
+                Toast.makeText(
+                    this@CameraActivity,
+                    "Gagal memunculkan kamera",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }, ContextCompat.getMainExecutor(this))
+
+        binding.btnSwitchCamera.setOnClickListener {
+            cameraSelector =
+                if (cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) CameraSelector.DEFAULT_FRONT_CAMERA
+                else CameraSelector.DEFAULT_BACK_CAMERA
+            startCamera()
+        }
+    }
+
+    private fun hideSystemUI() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    companion object {
+        const val IS_DETECTION = "is_detection"
+        const val IS_DISEASE_RIPE = "is_disease_ripe"
     }
 }
--- app/src/main/java/com/fari/agrovision/ui/camera/ImagePreviewActivity.kt
+++ app/src/main/java/com/fari/agrovision/ui/camera/ImagePreviewActivity.kt
@@ -1,12 +1,186 @@
 package com.fari.agrovision.ui.camera
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.fari.agrovision.R
+import com.fari.agrovision.databinding.ActivityImagePreviewBinding
+import com.fari.agrovision.ui.auth.AuthViewModel
+import com.fari.agrovision.ui.auth.AuthViewModelFactory
+import com.fari.agrovision.data.local.Result
+import java.io.File
 
 class ImagePreviewActivity : AppCompatActivity() {
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "auth")
+
+    private var uid: String? = null
+
+    private var isDetection = true
+    private var isDiseaseRipe = true
+    private var imageFile: File? = null
+
+    private lateinit var binding: ActivityImagePreviewBinding
+
+    private val authViewModel: AuthViewModel by viewModels {
+        AuthViewModelFactory.getInstance(dataStore)
+    }
+
+//    private val detectionViewModel: DetectionViewModel by viewModels {
+//        DetectionViewModelFactory.getInstance()
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_image_preview)
+        binding = ActivityImagePreviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        authViewModel.isLogin().observe(this) { uid ->
+            if (!uid.isNullOrEmpty()) {
+                this.uid = uid
+            }
+        }
+
+        getCondition()
+
+        setToolBar()
+        setPreviewImage()
+        setAction()
+    }
+
+    private fun getCondition() {
+        isDetection = intent.getBooleanExtra(IS_DETECTION, true)
+        isDiseaseRipe = intent.getBooleanExtra(IS_DISEASE_RIPE, true)
+    }
+
+    private fun setAction() {
+        binding.btnUpload.setOnClickListener {
+            if (isDetection) {
+//                postDetection(isDiseaseRipe)
+
+            } else {
+                uploadProfilePicture()
+            }
+        }
+    }
+
+//    private fun postDetection(isDiseaseRipe: Boolean) {
+//        if (!uid.isNullOrEmpty())
+//            if (isDiseaseRipe) {
+//                detectionViewModel.postDetectionDisease(uid!!, imageFile!!)
+//                    .observe(this) { result ->
+//                        when (result) {
+//                            is Result.Loading -> {
+//                                binding.progressBar.visibility = View.VISIBLE
+//                            }
+//                            is Result.Success -> {
+//                                binding.progressBar.visibility = View.GONE
+//                                val dataResult = result.data
+//                                val intent = Intent(
+//                                    this@ImagePreviewActivity,
+//                                    DetectionResultActivity::class.java
+//                                )
+//                                intent.putExtra(
+//                                    DetectionResultActivity.DETECTION_RESULT,
+//                                    dataResult
+//                                )
+//                                startActivity(intent)
+//                                finish()
+//                            }
+//                            is Result.Error -> {
+//                                binding.progressBar.visibility = View.GONE
+//                                Toast.makeText(
+//                                    this,
+//                                    "Failed to detect disease",
+//                                    Toast.LENGTH_SHORT
+//                                ).show()
+//
+//                            }
+//                        }
+//                    }
+//            } else
+//                Toast.makeText(
+//                    this,
+//                    "Failed to obtain user authentication",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+//
+//
+//    }
+
+    private fun uploadProfilePicture() {
+        if (!uid.isNullOrEmpty())
+            editProfilePicture(uid!!, imageFile!!)
+        else
+            Toast.makeText(
+                this,
+                "Failed to obtain user authentication",
+                Toast.LENGTH_SHORT
+            ).show()
+    }
+
+    private fun editProfilePicture(uid: String, imageFile: File) {
+        authViewModel.editProfilePicture(uid, imageFile).observe(this) { result ->
+            when (result) {
+                is Result.Loading -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                }
+                is Result.Success -> {
+                    Log.i("IMAGEPREVIEW", "ABCDEF BERHASIL ${result.data}")
+                    binding.progressBar.visibility = View.GONE
+                    AlertDialog.Builder(this).apply {
+                        setTitle("Success!")
+                        setMessage(result.data)
+                        setPositiveButton("Lanjut") { _, _ ->
+                            finish()
+                        }
+                        create()
+                        show()
+                    }
+                }
+                is Result.Error -> {
+                    binding.progressBar.visibility = View.GONE
+                    Toast.makeText(
+                        this,
+                        "Failed to update profile picture",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+            }
+        }
+    }
+
+    private fun setPreviewImage() {
+        val imagePath = intent.getStringExtra(IMAGE_PATH)
+        val imageUri = Uri.parse(imagePath)
+        imageFile = imageUri.path?.let { File(it) }
+        binding.ivPreviewImage.setImageURI(imageUri)
+
+    }
+
+    private fun setToolBar() {
+        binding.apply {
+            setSupportActionBar(toolbar.toolbar)
+            toolbar.tvToolbarTitle.text = getString(R.string.preview)
+            toolbar.btnBackToolbar.setOnClickListener {
+                finish()
+            }
+        }
+    }
+
+    companion object {
+        const val IS_DETECTION = "is_detection"
+        const val IS_DISEASE_RIPE = "is_disease_ripe"
+        const val IMAGE_PATH = "image_absolute_path"
     }
 }
